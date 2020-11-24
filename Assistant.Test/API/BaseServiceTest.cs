398ffc55b6002491c2d76819e3202629ee6e0127
Luis Gerardo <luisgpr99@gmail.com>
2020-11-23 21:42:24 -0600

    Creado Test para el API

---
+++ Assistant.Test/API/BaseServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Assistant.Test.API
+{
+    public class BaseServiceTest<T>
+    { 
+        public BaseServiceTest()
+        {
+            
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public static void Test<K>(K n1, K n2)
+        {
+            Assert.Equal(n1, n2);
+        }
+    }
+}
